f938bd44751fdbcad733b8464f298c79ca37b94f
plainspoken-kiran <kiran@plainspoken.com>
2023-10-03 01:31:32 -0400

    test

--- test.py
+++ test.py
@@ -1,22 +1,15 @@
 import requests
+import os
 
-# This is a fake API key for demonstration purposes. Never hard-code real API keys in your code!
-API_KEY = "add000c0f319b7f9d4f86c249ecabc30"
+# Assume API_KEY is stored as an environment variable for security
+API_KEY = os.environ.get("OPENWEATHER_API_KEY", "add000c0f319b7f9d4f86c249ecabc30")
 
-# Hypothetical API endpoint
-api_url = "https://api.example.com/data"
+# Example URL to get weather data for London
+api_url = f"http://api.openweathermap.org/data/2.5/weather?q=London&appid={API_KEY}"
 
-# Headers with API key
-headers = {
-    "Authorization": f"Bearer {API_KEY}"
-}
+response = requests.get(api_url)
 
-# Make a GET request to the API endpoint
-response = requests.get(api_url, headers=headers)
-
-# Check the status of the response
 if response.status_code == 200:
     print("Success:", response.json())
 else:
     print("Failed:", response.status_code)
-
