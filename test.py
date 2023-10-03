b153b828355bbb046a07e8a4b26de81740b7353d
plainspoken-kiran <kiran@plainspoken.com>
2023-10-03 01:42:18 -0400

    test2

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import os
 
 # Assume API_KEY is stored as an environment variable for security
 API_KEY = os.environ.get("OPENWEATHER_API_KEY", "add000c0f319b7f9d4f86c249ecabc30")
-
 # Example URL to get weather data for London
 api_url = f"http://api.openweathermap.org/data/2.5/weather?q=London&appid={API_KEY}"
 
